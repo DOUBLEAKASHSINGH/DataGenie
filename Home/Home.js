4c3c3a22803360aa8d30d4bd9bcd518d84ab117a
DOUBLEAKASHSINGH <doubleakashsingh@gmail.com>
2025-09-08 20:40:12 +0530

    Updating the DataGenie to the repo for the first time.

---
+++ Home/Home.js
@@ -0,0 +1,5 @@
+// Simple JS interaction
+document.getElementById("getStarted").addEventListener("click", () => {
+  alert("Welcome! Redirecting you to the dashboard...");
+  // window.location.href = "dashboard.html"; // later you can link it
+});
